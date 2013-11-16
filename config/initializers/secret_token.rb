543660a315fa3251a15b75bc44fcea5dfac9f0a4
Jem Nelson <pink.fwip@gmail.com>
2013-11-16 17:01:45 -0500

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Likeme::Application.config.secret_key_base = '402b1e5973217cb3730c7f44a05c3ecabde0fbd53807e633c0ad2d3acc685878e8c68f9ee55ccebdc37767551276c838ccc9c5ba7c39cc293c026198c3b9f405'
