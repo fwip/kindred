70614fa0190d9d99792ff3329d69ad0e0e7efdf7
Jem Nelson <pink.fwip@gmail.com>
2013-11-19 16:35:24 -0500

    Require topic to have a title

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,8 @@ class Topic < ActiveRecord::Base
   has_many :stories
   has_many :users, through: :stories
 
+  validates :title, presence: true, length: { minimum: 4, maximum: 80 }
+
   def self.getByTitle(title)
     topic = Topic.find_by(title: title)
     if topic.nil?
